9d143101313b370c47d70cb9fe92ed83cb4f0762
4a50 <jpjones22@gmail.com>
2021-05-24 13:43:30 -0700

    Basic Wiring Complete

---
+++ CosmicDeluxeAdventure/Data/CADDbContext.cs
@@ -0,0 +1,18 @@
+﻿using CosmicDeluxeAdventure.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CosmicDeluxeAdventure.Data
+{
+  public class CADDbContext : DbContext
+  {
+    public DbSet<UserInfo> TABLENAME { get; set; }
+    public CADDbContext(DbContextOptions<CADDbContext> options) : base(options)
+    {
+
+    }
+  }
+}
