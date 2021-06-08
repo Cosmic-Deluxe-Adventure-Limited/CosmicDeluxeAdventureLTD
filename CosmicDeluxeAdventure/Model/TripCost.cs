5037418296bf264370de4b69aef0f4f2a426d1d9
4a50 <jpjones22@gmail.com>
2021-06-08 10:57:07 -0700

    Added TripCost model

---
+++ CosmicDeluxeAdventure/Model/TripCost.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CosmicDeluxeAdventure.Model
+{
+  public class TripCost
+  {
+    [Required]
+    int ID { get; set; }
+    [Required]
+    int TripID { get; set; }
+    decimal Price { get; set; }
+    decimal Cost { get; set; }
+  }
+}
