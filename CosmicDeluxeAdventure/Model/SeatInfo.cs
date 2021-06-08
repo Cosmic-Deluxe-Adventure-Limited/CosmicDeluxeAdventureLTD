6d4070976d60b16ba3683085ee2a584b7b4754d7
4a50 <jpjones22@gmail.com>
2021-06-08 10:51:48 -0700

    Added Ship and SeatInfo Models

---
+++ CosmicDeluxeAdventure/Model/SeatInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace CosmicDeluxeAdventure.Model
+{
+  public class SeatInfo
+  {
+    [Required]
+    public int ID { get; set; }
+    public string SeatNumber { get; set; }
+    //status ENUM
+    
+    //Navigation Properties
+    public List<Flight> Flights { get; set; }
+
+  }
+}
---
+++ CosmicDeluxeAdventure/Model/Ship.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CosmicDeluxeAdventure.Model
+{
+  public class Ship
+  {
+    [Required]
+    public int ID {get; set;}
+    int ShipNumber { get; set; }
+    // Navigation Properties
+
+    List<Flight> Flghts { get; set; }
+    List<SeatInfo> Seats { get; set; }
+
+  }
+}
