3e8e4808a0b40e34de3c74e4325cd7e2b014082d
4a50 <jpjones22@gmail.com>
2021-06-08 10:55:11 -0700

    Added Trip model

---
+++ CosmicDeluxeAdventure/Model/Trip.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CosmicDeluxeAdventure.Model
+{
+  public class Trip
+  {
+    [Required]
+    public int ID { get; set; }
+    public int UserID { get; set; }
+    public int FlightID { get; set; }
+    public DateTime Added { get; set; }
+    
+    //status enum
+
+    //Navigation Properties
+    public List<UserInfo> UserInfo { get; set; }
+    public List<Flight> Flights { get; set; }
+
+  }
+}
