a54b56119bb296a72be770b2a4d2ebcb5826a12f
4a50 <jpjones22@gmail.com>
2021-07-06 12:53:50 -0700

    Database Data initial transfer complete.

--- CosmicDeluxeAdventure/ClientApp/src/components/FlightsPage/Flights.js
+++ CosmicDeluxeAdventure/ClientApp/src/components/FlightsPage/Flights.js
@@ -1,14 +1,30 @@
-import React from 'react';
+import React,  {useEffect } from 'react';
+//import { useState } from 'react';
 import { Card } from 'react-bootstrap';
+//import superagent from 'superagent';
+
 
 const Flights = () => {
-
+    useEffect(() => {
+        fetchData();
+    }, []);
+    //const [GetUserData, setUserData] = useState([]);
+    //let newUserData = superagent.get() 
+    
     return (
       <Card>
-      <Card.Body>Show me the flight options page</Card.Body>
+            <Card.Body>Show me the flight options page</Card.Body>
+
     </Card>
     );
-  }
+}
+
+const fetchData = async () => {
+   // const response = await fetch('home/GetData');
+   // const data = await response.json();
+    let jsonSampleData = [{ "id": 1, "firstName": "Fox", "lastName": "McCloud", "address": "1234 Corneria Way", "city": "Imperial Base", "state": "Alpha", "zipCode": 12345, "country": "Corneria", "phoneNumber": 1112223333, "userName": "barrelRoll1" }]
+    console.log(jsonSampleData);
+}
 
 
 export default Flights;
---
+++ CosmicDeluxeAdventure/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CosmicDeluxeAdventure.Model.Services;
+using CosmicDeluxeAdventure.Model.Interfaces;
+using CosmicDeluxeAdventure.Model;
+using System.Diagnostics;
+
+namespace CosmicDeluxeAdventure.Controllers
+{
+  [ApiController]
+  public class HomeController : ControllerBase //Inherit from Controller supports views.  ControllerBase does not support views
+  {
+    private readonly ILogger<HomeController> _logger;
+    private readonly IUserInfo _userInfo;
+    public HomeController(ILogger<HomeController> logger, IUserInfo userinfo)
+    {
+      _logger = logger;
+      _userInfo = userinfo;
+    }
+    [Route("{controller}/GetData")]
+    [HttpGet]
+    public async Task<IEnumerable<UserInfo>> Get()
+    {
+      Debug.WriteLine("GetData Hit");
+      return await _userInfo.GetAllUsers();
+    }
+  }
+}
