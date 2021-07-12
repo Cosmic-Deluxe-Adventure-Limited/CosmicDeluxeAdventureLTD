14adb2588b1ef8c6ae98400e8f21347305d26a07
4a50 <jpjones22@gmail.com>
2021-07-12 10:34:55 -0700

    Added Flight, Location and Ship Models

---
+++ CosmicDeluxeAdventure/Model/Flight.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CosmicDeluxeAdventure.Model
+{
+  public class Flight
+  {
+    [Required]
+    public int ID { get; set; }
+    [Required]
+    public DateTime Departure { get; set; }
+    [Required]
+    public DateTime Arrival { get; set; }
+    [Required]
+
+    public string Status { get; set; }
+    [Required]
+    public long Miles { get; set; }
+    //FK
+    public Ship ShipId { get; set; }
+    public Location Location {get; set;}
+  }
+}
---
+++ CosmicDeluxeAdventure/Model/Location.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CosmicDeluxeAdventure.Model
+{
+  public class Location
+  {
+    [Required]
+    int ID { get; set; }
+    [Required]
+    string Name { get; set; }
+    [Required]
+    string Description { get; set; }
+  }
+}
+
