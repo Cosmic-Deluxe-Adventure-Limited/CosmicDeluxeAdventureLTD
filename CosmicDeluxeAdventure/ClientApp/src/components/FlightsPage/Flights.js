c36d3a9e1f20d4208d8f204ebc82539396a2e5b4
4a50 <jpjones22@gmail.com>
2021-08-13 18:02:17 -0700

    re-factored flight data fetching

--- CosmicDeluxeAdventure/ClientApp/src/components/FlightsPage/Flights.js
+++ CosmicDeluxeAdventure/ClientApp/src/components/FlightsPage/Flights.js
@@ -1,41 +1,35 @@
 import React,  {useEffect } from 'react';
-//import { useState } from 'react';
+import { useState } from 'react';
 import { Card } from 'react-bootstrap';
 //import superagent from 'superagent';
 
 
-const Flights = () => {
+ 
+
+const Flights = (props) => {
+
+    const [flightData, setFlightData] = useState({});
+    const [allUsers, setAllUsers] = useState([]);
     useEffect(() => {
         fetchData();
-    }, []);
-    //const [GetUserData, setUserData] = useState([]);
-    //let newUserData = superagent.get() 
-    
-    return (
+    }, []);    
+const fetchData = () => {        
+    const flightResponse = fetch('home/flights/getFlight/1')
+        .then(data => data.json())
+        .then(data => setFlightData(data))
+        .catch(err => console.log(err));   
+    return flightResponse;
+    }    
+    return (        
+        <>
+            <h1>{flightData.arrival}</h1>
         <Card>
-
             <Card.Body>Show me the flight options page</Card.Body>
 
-    </Card>
+            </Card>
+            </>
     );
-}
-
-const fetchData = async () => {
-    //const response = await fetch('home/GetData');
-    //const data = await response.json();
-    //let jsonSampleData = [{ "id": 1, "firstName": "Fox", "lastName": "McCloud", "address": "1234 Corneria Way", "city": "Imperial Base", "state": "Alpha", "zipCode": 12345, "country": "Corneria", "phoneNumber": 1112223333, "userName": "barrelRoll1" }]
-    //console.log(jsonSampleData);
-    try {
-        const flightResponse = await fetch('home/flights/getFlight/1')
-            .then(resp => resp.json());
-        console.log(flightResponse);
-        //const flightRespData = await flightResponse.json();
-        //console.log(flightResponse);
-        //console.log(flightRespData);
-    }
-    catch (err) {
-        console.error('error fetching!', err);
-    }
+    
 }
 
 
